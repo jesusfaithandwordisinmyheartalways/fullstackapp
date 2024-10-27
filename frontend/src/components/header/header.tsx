0285b7a0ba5c2ef44e17ee24cc1feb54b689416e
andrewjohnson <andrewjohnson9393@gmail.com>
2024-10-26 23:16:39 -0500

    files

---
+++ frontend/src/components/header/header.tsx
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+import React from 'react'
+import './header.css'
+
+
+
+const Header:React.FC = () => {
+
+
+    const HeaderClick = () => {
+        alert('Please fill out the form to receive shipping information')
+    }
+
+  return (
+    <>
+    <div className='header-container' onClick={HeaderClick}>
+        <div className='header-wrapper'>
+            <div><div><h3>Free Shipping on All Orders</h3></div></div>
+
+
+        </div>
+    </div>
+    
+    
+    
+    </>
+  )
+}
+
+
+
+
+
+export default Header
---
+++ frontend/src/components/shop/shop.tsx
@@ -0,0 +1,75 @@
+
+
+
+
+
+import React from 'react'
+import './shop.css'
+import GuyDrink from '../images/guysdrinking.webp'
+import WomanDrink from '../images/woman drinking.webp'
+import ManDrink from '../images/mandrinkning.jpg'
+
+
+const Shop:React.FC = () => {
+
+        const Instagram = (url:string):void => {
+             window.location.href = url;
+        }
+
+  return (
+  <>
+  
+  <div className='Shop-Container'>
+    <div className='Shop-Wrapper'>
+        <div><div><h3>FANS ARE EXCITED</h3></div></div>
+    </div>
+
+  </div>
+  
+  
+     <div className='Shop-Container-Two'>
+
+     <div className='Shop-Wrapper-Two'>
+        <div><div><h3>TAG US</h3></div></div><div><div><div className='Tag-Beverage' onClick={() => Instagram('https://www.instagram.com/')}>   @ BEVERAGE MARKETPLACE</div></div></div>
+    </div>
+
+
+     </div>
+
+
+
+     <div className='Shop-Container-Three'>
+        <div className='Shop-Wrapper-Three'>
+
+            <div><div><img src={GuyDrink}></img></div></div>
+
+            <div><div><img src={ WomanDrink}></img></div></div>
+
+            <div><div><img src={ManDrink}></img></div></div>
+
+            
+
+
+        </div>
+
+
+
+     </div>
+
+
+
+
+
+
+      
+
+
+  
+  
+  </>  
+
+
+  )
+}
+
+export default Shop
